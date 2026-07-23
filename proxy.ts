5840b383f9444012fc43276ffc1b6a4fde16d354
Imran Hossain <imranhossen0586@gmail.com>
2026-07-23 23:45:05 +0600

    add nextjs proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,32 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest) {
-    console.log(request)
+
+const AUTH_ROUTES = ["/login", "/register"];
+
+export async function proxy(request: NextRequest) {
+
+    const pathname = request.nextUrl.pathname;
+
+    const cookieStore = await cookies();
+
+    let aceessToken = request.cookies.get("accessToken")?.value;
+    let refreshToken = request.cookies.get("refreshToken")?.value;
+
+
+    let decodedAccessToken = accessToken ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string) : null;
+
+    const decodedRefreshToken = refreshToken ? jwtUtils.verifyToken(refreshToken, process.env.JWT_REFRESH_SECRET as string) : null;
 
     return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
     matcher: [
-        '/dashboard/:path*',
-        '/admin-dashboard/:path*',
-        '/author-dashboard/:path*',
+        // '/dashboard/:path*',
+        // '/admin-dashboard/:path*',
+        // '/author-dashboard/:path*',
+        '/((?!api|_next/static|_next/image|.*\\.png$).*)',
     ]
 }
